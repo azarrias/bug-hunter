abb09d71d244dc2d1ce97b100c9770bda42e0a35
azarrias <adolfo.zarrias@gmail.com>
2020-08-10 12:23:14 +0200

    Turn to use sprite batches for the tile set

--- src/Level.lua
+++ src/Level.lua
@@ -1,7 +1,9 @@
 Level = Class{}
 
 function Level:init()
-  self.tileMap = self:CreateTileMap(50, 50)
+  self.rows = 50
+  self.columns = 50
+  self.tilemap = TileMap(50, 50)
   self.player = self:CreatePlayer()
 end
 
@@ -10,7 +12,7 @@ function Level:update(dt)
 end
 
 function Level:render()
-  self.tileMap:render()
+  self.tilemap:render()
   self.player:render()
 end
 
@@ -22,31 +24,4 @@ function Level:CreatePlayer()
   player:AddComponent(playerSprite)
   
   return player
-end
-  
-function Level:CreateTileMap(columns, rows)
-  local tilemap = TileMap(columns, rows)
-  
-  for y = 1, rows do
-    table.insert(tilemap.baseTiles, {})
-    table.insert(tilemap.grassTiles, {})
-    
-    for x = 1, columns do
-      local baseTile = tiny.Entity(x * ENTITY_SIZE.x - ENTITY_SIZE.x / 2,
-        y * ENTITY_SIZE.y - ENTITY_SIZE.y / 2)
-      local grassTile = tiny.Entity(x * ENTITY_SIZE.x - ENTITY_SIZE.x / 2,
-        y * ENTITY_SIZE.y - ENTITY_SIZE.y / 2)
-      
-      local baseSprite = tiny.Sprite(TEXTURES['tiles'], QUADS['tiles-grass'][math.random(#QUADS['tiles-grass'])])
-      local quad = y > 10 and QUADS['tile-bush'][1] or QUADS['tile-empty'][1]
-      local grassSprite = tiny.Sprite(TEXTURES['tiles'], quad)
-      baseTile:AddComponent(baseSprite)
-      grassTile:AddComponent(grassSprite)
-      
-      table.insert(tilemap.baseTiles[y], baseTile)
-      table.insert(tilemap.grassTiles[y], grassTile)
-    end
-  end
-  
-  return tilemap
 end
--- src/TileMap.lua
+++ src/TileMap.lua
@@ -1,17 +1,65 @@
 TileMap = Class{}
 
+local TILE_ID_GRASS = {1, 2}
+local TILE_ID_EMPTY = 3
+local TILE_ID_BUSH = 4
+
 function TileMap:init(width, height)
   self.width = width
   self.height = height
   self.baseTiles = {}
   self.grassTiles = {}
+  self.baseSpriteBatch = nil
+  self.grassSpriteBatch = nil
+  self.tileQuads = {}
+  
+  self:CreateMap()
+  self:CreateTileset()
+  self:UpdateTileset()
+end
+
+function TileMap:CreateMap()
+  for y = 1, self.height do
+    table.insert(self.baseTiles, {})
+    table.insert(self.grassTiles, {})
+    
+    for x = 1, self.width do
+      local tile = math.random(2)
+      table.insert(self.baseTiles[y], tile)
+      tile = y > 10 and TILE_ID_BUSH or TILE_ID_EMPTY
+      table.insert(self.grassTiles[y], tile)
+    end
+  end
+end
+
+function TileMap:CreateTileset()
+  self.tileQuads[1] = QUADS['tiles-grass'][1]
+  self.tileQuads[2] = QUADS['tiles-grass'][2]
+  self.tileQuads[TILE_ID_EMPTY] = QUADS['tile-empty'][1]
+  self.tileQuads[TILE_ID_BUSH] = QUADS['tile-bush'][1]
+
+  self.baseSpriteBatch = love.graphics.newSpriteBatch(TEXTURES['tiles'], self.width * self.height)
+  self.grassSpriteBatch = love.graphics.newSpriteBatch(TEXTURES['tiles'], self.width * self.height)
+end
+
+function TileMap:UpdateTileset()
+  self.baseSpriteBatch:clear()
+  self.grassSpriteBatch:clear()
+  
+  for y = 1, self.height do
+    for x = 1, self.width do
+      -- TODO: Add only visible tile quads to the sprite batches
+      self.baseSpriteBatch:add(self.tileQuads[self.baseTiles[y][x]], (x - 1) * TILE_SIZE.x, (y - 1) * TILE_SIZE.y)
+      self.grassSpriteBatch:add(self.tileQuads[self.grassTiles[y][x]], (x - 1) * TILE_SIZE.x, (y - 1) * TILE_SIZE.y)
+    end
+  end
+  
+  self.baseSpriteBatch:flush()
+  self.grassSpriteBatch:flush()
 end
 
 function TileMap:render()
-    for y = 1, self.height do
-        for x = 1, self.width do
-            self.baseTiles[y][x]:render()
-            self.grassTiles[y][x]:render()
-        end
-    end
+  love.graphics.draw(self.baseSpriteBatch)
+  love.graphics.draw(self.grassSpriteBatch)
+  love.graphics.print("FPS: "..love.timer.getFPS(), VIRTUAL_SIZE.x - 35, VIRTUAL_SIZE.y - 10)
 end
